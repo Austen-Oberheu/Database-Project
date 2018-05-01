3bfe26f5822973a5345bb276d8d503a73a29cc44
Austen <Austen.Oberheu@me.com>
2018-05-01 15:19:35 -0400

    Final Commit
    Added Payroll Functionality

--- Database Project/Database_Project.py
+++ Database Project/Database_Project.py
@@ -1,11 +1,13 @@
 import sys
+import re
 import PyQt5
 from PyQt5.QtWidgets import (QApplication, qApp, QAction, QDesktopWidget, QToolTip, 
 							 QPushButton, QMessageBox, QMainWindow, QMenu, QTextEdit,
 							 QHBoxLayout, QVBoxLayout, QLineEdit, QTableWidget, 
-							 QTableWidgetItem, QLabel, QWidget, QComboBox, QDateEdit)
+							 QTableWidgetItem, QLabel, QWidget, QComboBox, QDateEdit,
+							 QCheckBox)
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import pyqtSlot, QDate
+from PyQt5.QtCore import pyqtSlot, QDate, QTimer
 from PyQt5.QtSql import *
 
 	
@@ -24,7 +26,173 @@ def is_number(s):
 
 	return False
 
-class CreateWindow(QWidget):
+class SalaryWindow(QWidget):
+    
+	def __init__(self, employeeNum):
+		super().__init__()
+		self.initUI()
+		self.initInfo(employeeNum)
+		self.timer = QTimer(self)
+		self.timer.timeout.connect(self.updatePay)
+		self.timer.start(1000)
+
+
+	def initInfo(self, employeeNum):
+		query = QSqlQuery()
+		ok = query.exec('SELECT employee_num, first_name, last_name, s.dept_name, e.position, pay_rate, hours, payment '
+						'FROM employee e '
+						'LEFT JOIN '
+						'salary as s on e.employee_num = s.emp_num '
+						'WHERE e.employee_num = {0}'.format(int(employeeNum)))
+		if (ok == False):
+			print(query.lastQuery())
+			print(query.lastError().number())
+			print(query.lastError().databaseText())
+		else:
+			print('Successful')
+	
+		while (query.next()):
+			firstName = query.value(1)
+			lastName = query.value(2)
+			deptNum = query.value(3)
+			salary = str(query.value(4))
+			payRate = query.value(5)
+			hours = query.value(6)
+			payment = query.value(7)
+			
+
+		self.firstNameBox.insert(firstName)
+		self.lastNameBox.insert(lastName)
+		self.idBox.insert(employeeNum)
+		self.deptbox.insert(str(deptNum))
+
+		self.payRatBox.insert(str(payRate))
+
+		isSalary = None
+		if (re.search('Manager', salary)):
+				isSalary = True
+		else:
+				isSalary = False
+
+		print(isSalary)
+
+		self.salaryCheckBox.setChecked(isSalary)
+		self.hoursBox.insert(str(hours))
+
+		if (isSalary):
+			self.paymentBox.insert(str((payRate / 12) / 2))
+		else:
+			self.paymentBox.insert(str(hours * payRate))
+
+	def initUI(self):
+		self.setGeometry(300, 300, 600, 300)
+		self.setWindowTitle('Employee Pay')
+
+		self.deptlabel = QLabel(self)
+		self.deptlabel.setText('Department')
+		self.deptlabel.move(25, 20)
+		self.deptbox = QLineEdit(self)
+		self.deptbox.move(25, 45)
+		self.deptbox.resize(125, 20)
+		self.deptbox.setMaxLength(25) 
+		self.deptbox.setReadOnly(True)
+
+		self.idLabel = QLabel(self)
+		self.idLabel.setText('ID')
+		self.idLabel.move(165, 20)
+		self.idBox = QLineEdit(self)
+		self.idBox.move(165, 45)
+		self.idBox.resize(40, 20)
+		self.idBox.setReadOnly(True)
+
+		self.firstNameLable = QLabel(self)
+		self.firstNameLable.setText('First Name')
+		self.firstNameLable.move(215, 20)
+		self.firstNameBox = QLineEdit(self)
+		self.firstNameBox.move(215, 45)
+		self.firstNameBox.resize(80, 20)
+		self.firstNameBox.setReadOnly(True)
+		#Create the box for Last Name results
+		self.lastNameLable = QLabel(self)
+		self.lastNameLable.setText('Last Name')
+		self.lastNameLable.move(315, 20)
+		self.lastNameBox = QLineEdit(self)
+		self.lastNameBox.move(315, 45)
+		self.lastNameBox.resize(80, 20)
+		self.lastNameBox.setReadOnly(True)
+		
+		self.payRateLabel = QLabel(self)
+		self.payRateLabel.setText('Pay Rate')
+		self.payRateLabel.move(25, 75)
+		self.payRatBox = QLineEdit(self)
+		self.payRatBox.move(25, 95)
+		self.payRatBox.resize(80, 20)
+
+		self.salaryLabel = QLabel(self)
+		self.salaryLabel.setText('Salary')
+		self.salaryLabel.move(125, 75)
+		self.salaryCheckBox = QCheckBox(self)
+		self.salaryCheckBox.move(125, 95)
+
+		self.hoursLabel = QLabel(self)
+		self.hoursLabel.setText('Hours')
+		self.hoursLabel.move(165, 75)
+		self.hoursBox = QLineEdit(self)
+		self.hoursBox.move(165, 95)
+		self.hoursBox.resize(80, 20)
+
+		self.paymentLabel = QLabel(self)
+		self.paymentLabel.setText('Payment')
+		self.paymentLabel.move(265, 75)
+		self.paymentBox = QLineEdit(self)
+		self.paymentBox.move(265, 95)
+		self.paymentBox.resize(80, 20)
+
+		self.dbLabel = QLabel(self)
+		self.dbLabel.setText('						')
+		self.dbLabel.move(165, 155)		
+		self.button = QPushButton('Save Changes', self)
+		self.button.resize(120, 20)
+		self.button.move(25, 150)
+		self.button.clicked.connect(self.save_click)
+		#Create the box for returned results and the label that goes above them
+		
+		#self.deptbox = QLineEdit(self)
+		#self.deptbox.move(125, 100)
+		#self.deptbox.resize(125, 20)
+		#self.deptbox.setMaxLength(25)
+		
+		self.show()
+
+	@pyqtSlot()
+
+	
+	def save_click(self):
+		query = QSqlQuery()
+		ok = query.exec('UPDATE s '
+						'SET pay_rate = {0}0, hours = {1}0, payment = {2}0 '
+						'FROM salary s '
+						'WHERE emp_num = {3}'
+						.format(round(float(self.payRatBox.text()), 2), round(float(self.hoursBox.text()), 2), round(float(self.paymentBox.text()), 2), int(self.idBox.text())))
+		
+		if (ok == False):
+			self.dbLabel.setText('Employee Pay Save Failed')
+			print(query.lastQuery())
+			print(query.lastError().number())
+			print(query.lastError().databaseText())
+		else:
+			self.dbLabel.setText('Employee Pay Saved')
+			
+	def updatePay(self):
+		hours = float(self.hoursBox.text())
+		payRate = float(self.payRatBox.text())
+		self.paymentBox.clear()
+		if (self.salaryCheckBox.isChecked()):
+			self.paymentBox.insert(str(round(((payRate / 12) / 2), 2)))
+		else:
+			self.paymentBox.insert(str(hours * payRate))
+
+class AddEmployeeWindow(QWidget):
     
 	def __init__(self):
 		super().__init__()
@@ -165,9 +333,10 @@ class SQL(QMainWindow):
 		self.button.move(20, 20)
 		self.button.clicked.connect(self.on_click)
 #Create the textbox for searches
-		self.textbox = QTextEdit(self)
+		self.textbox = QLineEdit(self)
 		self.textbox.move(20, 45)
-		self.textbox.resize(280, 25)
+		self.textbox.resize(280, 20)
+		self.textbox.setPlaceholderText('Enter First Name, Last Name, or ID Number')
 #Employee ID Box
 		self.idLabel = QLabel(self)
 		self.idLabel.setText('ID')
@@ -237,8 +406,13 @@ class SQL(QMainWindow):
 
 		self.addButton = QPushButton('Add', self)
 		self.addButton.resize(90, 20)
-		self.addButton.move(555, 140)
+		self.addButton.move(655, 140)
 		self.addButton.clicked.connect(self.addEvent)
+
+		self.addButton = QPushButton('Pay', self)
+		self.addButton.resize(90, 20)
+		self.addButton.move(555, 140)
+		self.addButton.clicked.connect(self.payEvent)
 
 		self.updatelabel = QLabel(self)
 		self.updatelabel.setText('')
@@ -262,13 +436,16 @@ class SQL(QMainWindow):
 		self.show()
 
 	def closeEvent(self, event):
-		self.db.close()
+		if(self.dbLabel.close() is None):
+			self.db.close()
 
 
 #Connects to the database with the credentials below when the button is clicked
 	@pyqtSlot()
 
 	def deleteEvent(self):
+		if(self.idBox.text() == ''):
+			return
 		choice = QMessageBox.question(self, 'Extract!',
                                             "Delete the Entry?",
                                             QMessageBox.Yes | QMessageBox.No)
@@ -290,7 +467,13 @@ class SQL(QMainWindow):
 				print(query.lastError().databaseText())
 
 	def addEvent(self):
-		self.window = CreateWindow()
+		self.window = AddEmployeeWindow()
+
+	def payEvent(self):
+		if(self.idBox.text() == ''):
+			return
+		employeeNum = self.idBox.text()
+		self.window = SalaryWindow(employeeNum)
 
 	def on_click(self):
 		self.db = QSqlDatabase.addDatabase('QODBC')
@@ -309,7 +492,7 @@ class SQL(QMainWindow):
 #Runs a query searching for a row that matches the first or last name, currently not sure how it would handle multiple rows returning		
 	def query_click(self):
 		query = QSqlQuery()
-		queryInput = self.textbox.toPlainText()
+		queryInput = self.textbox.text()
 		if(is_number(queryInput) == True):
 			ok = query.exec('SELECT first_name, last_name, bday, employee_num, gender, date_started, department.dept_name FROM employee LEFT JOIN department ON employee.dept_num = department.dept_num WHERE employee_num = \'{0}\' '.format(queryInput))
 		else:
@@ -345,6 +528,8 @@ class SQL(QMainWindow):
 			self.updatelabel.setText('Query Successful')
 
 	def update_click(self):
+		if(self.idBox.text() == ''):
+			return
 		query = QSqlQuery()
 		ok = query.exec('UPDATE e ' 
 			'SET first_name = \'{0}\', last_name = \'{1}\', bday = \'{2}\', employee_num = \'{3}\', gender = \'{4}\', date_started = \'{5}\' , dept_num = (SELECT dept_num FROM department WHERE dept_name = \'{6}\') '
